d77939eeaaa54d71c93bd31a2abf04185ed973fb
Courdevelops <courdevelops@gmail.com>
2023-06-24 18:48:23 -0400

    menu there but not sliding

--- slidemenu.js
+++ slidemenu.js
@@ -2,7 +2,7 @@
 
 window.onload = function () {
     window.addEventListener('scroll', function (e) {
-        console.log("Do I need this?");
+       console.log("Do I need this?");
     });
 
     //////////////////////// MOBILE MENU SCRIPT /////////////////////////////////
@@ -20,6 +20,23 @@ window.onload = function () {
         home_burger_mobile_main.classList.toggle('is-active');
         // toggle is active css state for menu to slide in
         home_slide_menu_mobile_main.classList.toggle('is-active');
+    });
+
+
+    // ABOUT SECTION
+
+    // ABOUT BURGER MOBILE MAIN QUERY
+    const about_burger_mobile_main = document.querySelector('.hamburgermobileaboutmain');
+    // SLIDE ABOUT MENU QUERY
+    const about_slide_menu_mobile_main = document.querySelector('.nav-mobile-main-about');
+
+    // Adding Eventlistener to the hamburger and slide menu
+    about_burger_mobile_main.addEventListener('click', function () {
+        // toggle is active css state for hamburger menu
+        about_burger_mobile_main.classList.toggle('is-active');
+        // toggle is active css state for menu to slide in
+        about_slide_menu_mobile_main.classList.toggle('is-active');
+        console.log("about burger pressed")
     });
 }
 
